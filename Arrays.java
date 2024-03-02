6db00929d0f86ed595fe4285f8e944fd6f902b69
raghab007 <pokhrelraghab3@gmail.com>
2024-03-02 12:19:23 +0545

    Arrrays syntax

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // Syntax
+        // datatype[] variable_name = new datatype[size];
+        int [] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1]  = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+        System.out.println(numbers); // It will print address of the array
+
+
+    }
+    
+}
